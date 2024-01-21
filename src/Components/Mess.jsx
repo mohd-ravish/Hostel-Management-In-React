95ccbf7782c8cc3da7930203ed7b85254ef10e05
mohd-ravish <mohdravish2003@gmail.com>
2024-01-21 14:10:31 +0530

    Mess Section Added

--- src/Components/Mess.jsx
+++ src/Components/Mess.jsx
@@ -1,56 +1,175 @@
 import Header from "./Header";
 import Footer from "./Footer";
 
-
-
 function Mess() {
     return (
         <div>
             <Header />
             <div class="fee feeSchedule">
                 <h1>Minimum Expected Menu</h1>
-                <table class="table table-bordered">
-                    <thead class="table-dark">
-                        <td></td>
-                        <td>Saturday</td>
-                    </thead>
-                    <tbody>
-                        <tr class="table">
-                            <td>Breakfast</td>
-                            <td>Brown Bread(3)<br></br>Amul Butter(1/6) of 100gm /Tops Fruit Jam / Boiled Egg<br></br>Tea</td>
-                        </tr>
-                        <tr class="table-active">
-                            <td>Lunch</td>
-                            <td>Aalu Palak / Seasonal Veg<br></br>Rice<br></br>Daal<br></br>Roti<br></br>Salad(Onion/Gajar/Muli/Kheera)</td>
-                        </tr>
-                        <tr class="table">
-                            <td>Dinner</td>
-                            <td>Chicken Biriany<br></br>Raita(Boondi)<br></br>Mutter Paneer<br></br>Pulao<br></br>Raita(Boondi)</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <table class="table table-bordered">
-                    <thead class="table-dark">
-                        <td></td>
-                        <td>Sunday</td>
-                    </thead>
-                    <tbody>
-                        <tr class="table">
-                            <td>Breakfast</td>
-                            <td>Anda Paratha(2), Chatni, Tea, Aalu Paratha(2), Chatni, Tea</td>
-                        </tr>
-                        <tr class="table-active">
-                            <td>Lunch</td>
-                            <td>Mutter Paneer, Rice, Daal, Roti, Salad(Onion/Gajar/Muli/Kheera)</td>
-                        </tr>
-                        <tr class="table">
-                            <td>Dinner</td>
-                            <td>Chicken Biriany, Raita (Boondi), Mutter Paneer, Pulao, Raita (Boondi)</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="mess-table">
+                    <table class="table table-bordered">
+                        <thead class="table-dark">
+                            {/* <td></td> */}
+                            <td>Sunday</td>
+                        </thead>
+                    </table>
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="table">
+                                <td>Breakfast</td>
+                                <td>Anda Paratha(2), Chatni, Tea, Aalu Paratha(2), Chatni, Tea</td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>Lunch</td>
+                                <td>Mutter Paneer, Rice, Daal, Roti, Salad(Onion/Gajar/Muli/Kheera)</td>
+                            </tr>
+                            <tr class="table">
+                                <td>Dinner</td>
+                                <td>Chicken Biriany, Raita (Boondi), Mutter Paneer, Pulao, Raita (Boondi)</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="mess-table">
+                    <table class="table table-bordered">
+                        <thead class="table-dark">
+                            <td>Monday</td>
+                        </thead>
+                    </table>
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="table">
+                                <td>Breakfast</td>
+                                <td>Brown Bread(3), Amul Butter(1/6) of 100gm /Tops Fruit Jam / Boiled Egg(1), Tea</td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>Lunch</td>
+                                <td>Aalu Chhole ,Rice, Daal, Roti, Salad (Onion/Gajar/Muli/Kheera)</td>
+                            </tr>
+                            <tr class="table">
+                                <td>Dinner</td>
+                                <td>Chicken Qorma/Chicken Strew, Roti, Rice, Daal, Mutter Paneer, Ruti, Rice, Daal</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="mess-table">
+                    <table class="table table-bordered">
+                        <thead class="table-dark">
+                            <td>Tuesday</td>
+                        </thead>
+                    </table>
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="table">
+                                <td>Breakfast</td>
+                                <td>Puri(6), Sabzi, Tea</td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>Lunch</td>
+                                <td>Lauki Chana Daal, Rice, Daal, Roti, Salad (Onion/Gajar/Muli/Kheera)</td>
+                            </tr>
+                            <tr class="table">
+                                <td>Dinner</td>
+                                <td>Mix Veg/Seasonal Veg(Dry), Daal, Roti, Rice</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="mess-table">
+                    <table class="table table-bordered">
+                        <thead class="table-dark">
+                            <td>Wednesday</td>
+                        </thead>
+                    </table>
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="table">
+                                <td>Breakfast</td>
+                                <td>Brown Bread(2), Omelette(2 egg), Tea, Brown Bread(2), Jam/Butter, Tea</td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>Lunch</td>
+                                <td>Rajma Aaalu, Rice, Daal, Roti, Salad</td>
+                            </tr>
+                            <tr class="table">
+                                <td>Dinner</td>
+                                <td>Chicken Biriyani, Raita (Boondi), Mutter Paneer, Pulao, Raita (Boondi)</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="mess-table">
+                    <table class="table table-bordered">
+                        <thead class="table-dark">
+                            <td>Thursday</td>
+                        </thead>
+                    </table>
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="table">
+                                <td>Breakfast</td>
+                                <td>Rusk (4), Banana (1), Boiled Egg (1), Tea</td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>Lunch</td>
+                                <td>Mix Vegetable, Rice, Daal, Roti, Salad</td>
+                            </tr>
+                            <tr class="table">
+                                <td>Dinner</td>
+                                <td>Egg Curry, Rice, Daal, Roti, Kheer/Halwa, Aalu Tamatar/Seasonal Veg, Rice, Daal, Roti, Kheer/Halwa</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="mess-table">
+                    <table class="table table-bordered">
+                        <thead class="table-dark">
+                            <td>Friday</td>
+                        </thead>
+                    </table>
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="table">
+                                <td>Breakfast</td>
+                                <td>Aalu Paratha (2), Chatni, Tea</td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>Lunch</td>
+                                <td>ahiri/Veg. Pulao, Raita (Boondi), Papad 1 pc</td>
+                            </tr>
+                            <tr class="table">
+                                <td>Dinner</td>
+                                <td>Chicken Qorma, Roti, Zeera Rice, Daal, Gulab Jamun(2)/Fruit Cream, Palak Paneer, Roti, Zeera Rice, Daal, Gulab Jamun(2)/Fruit Cream</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="mess-table">
+                    <table class="table table-bordered">
+                        <thead class="table-dark">
+                            <td>Saturday</td>
+                        </thead>
+                    </table>
+                    <table class="table table-bordered">
+                        <tbody>
+                            <tr class="table">
+                                <td>Breakfast</td>
+                                <td>Brown Bread(3), Amul Butter(1/6) of 100gm /Tops Fruit Jam / Boiled Egg, Tea</td>
+                            </tr>
+                            <tr class="table-active">
+                                <td>Lunch</td>
+                                <td>Aalu Palak / Seasonal Veg, Rice, Daal, Roti, Salad(Onion/Gajar/Muli/Kheera)</td>
+                            </tr>
+                            <tr class="table">
+                                <td>Dinner</td>
+                                <td>Chicken Biriany, Raita(Boondi), Mutter Paneer, Pulao, Raita(Boondi)</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
             <Footer />
         </div>
     );
